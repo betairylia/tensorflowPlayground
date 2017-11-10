6c885f684330650489368b69378f1590e9b39ae6
Betairya Sapphire <betairya@gmail.com>
2017-11-10 22:17:28 +0900

    Updated MIMLP

--- README.md
+++ README.md
@@ -2,3 +2,34 @@
 Just a playground.
 
 Full of happiness (?)
+
+## MLP Mutual Information
+
+A naive Representation of paper _Opening the Black Box of Deep Neural Networks via Information_.  
+(SimpleMLP_wMICalc_DataGen.py)
+
+MLP: 12 (Input) - 12 - 10 - 8 - 6 - 5 - 4 (Output)  
+Batchsize: 150  
+Learning rate: 0.1 (fixed)  
+Data set size: 300000  
+Learning Loops: 20 (50 in original paper)  
+Optimizer: SGD (without momentum)
+
+### Results:  
+The MI Plane plot (darker = more iteration):
+![](Images\MIPlane_MLP_I12_12_10_8_6_5_O4_30wData_150Minibatch_lr0.1.png)  
+
+Zoom Up:  
+![](Images\MIPlane_MLP_I12_12_10_8_6_5_O4_30wData_150Minibatch_lr0.1_Layer123Local.png)
+
+
+**Change the MLP to: ** 12 - 10 - 8 - 6 - **6** :  
+![](Images\MIPlane_MLP_I12_12_10_8_6_6_O4_30wData_150Minibatch_lr0.1.png)
+
+**Change the MLP to: ** 12 - 10 - **6 - 10 - 6** :  
+![](Images\MIPlane_MLP_I12_12_10_6_10_6_O4_30wData_150Minibatch_lr0.1.png)
+
+
+>Ref:
+
+>Shwartz-Ziv, Ravid, and Naftali Tishby. "Opening the Black Box of Deep Neural Networks via Information." arXiv preprint arXiv:1703.00810 (2017).
--- SimpleMLP_wMICalc_DataGen.py
+++ SimpleMLP_wMICalc_DataGen.py
@@ -1,26 +1,48 @@
 import numpy as np
 import tensorflow as tf
 import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+from sklearn.metrics import mutual_info_score
+from termcolor import colored, cprint
 
 #Global config variables
-dataCount = 1000
+dataCount = 300000
 numEpochs = 1
-batch_size = 100
+batch_size = 150
 learning_rate = 0.1
 
+MI_Interval = 40
+num_loop = 20
+
 #Network creation
-numInputs = 1
-numOutputs = 2
-layerStructure = [numInputs, 8, 6, numOutputs] #Input -> hidden -> ... -> Output
+numInputs = 12
+numOutputs = 4
+layerStructure = [numInputs, 12, 10, 6, 10, 6, numOutputs] #Input -> hidden -> ... -> Output
+
+def get_Y(input, inputLenth, classCount):
+    stepLenth = inputLenth // classCount
+    targetClass = 0
+    maxArrayNum = 0
+    for i in range(classCount):
+        n = array_to_number(input[i * stepLenth : (i + 1) * stepLenth])
+        if(maxArrayNum <= n):
+            maxArrayNum = n
+            targetClass = i
+
+    result = []
+    for i in range(classCount):
+        if i == targetClass:
+            result.append(1)
+        else:
+            result.append(0)
+
+    return result
 
 def gen_data(size=1000):
     X = np.array(np.random.choice(2, size=(size,numInputs)))
     Y = []
     for i in range(size):
-        if X[i] == 1:
-            Y.append([1, 0])
-        else:
-            Y.append([0, 1])
+        Y.append(get_Y(X[i], numInputs, numOutputs))
     return X, np.array(Y)
 
 # adapted from https://github.com/tensorflow/tensorflow/blob/master/tensorflow/models/rnn/ptb/reader.py
@@ -51,16 +73,72 @@ def array_to_number(a):
         result *= 2
         if(a[len(a) - i - 1] > 0):
             result += 1
+    return result
 
-def calculateMI_Plane(x, t, y, binCount = 16):
+def calculate_prob(a):
+    dict_Index = dict()
+    count = np.zeros(a.size)
+
+    for i in range(a.size):
+        if a[i] not in dict_Index.keys():
+            dict_Index[a[i]] = len(dict_Index)
+        count[dict_Index[a[i]]] += 1
+
+    prob = np.zeros(len(dict_Index))
+
+    for i in range(len(dict_Index)):
+        prob[i] = count[i] / a.size
+
+    return prob
+
+def calculate_jointProb(a, b):
+    dict_IndexA = dict()
+
+    for i in range(a.size):
+        if a[i] not in dict_IndexA.keys():
+            dict_IndexA[a[i]] = len(dict_IndexA)
+
+    ####
+
+    dict_IndexB = dict()
+
+    for i in range(b.size):
+        if b[i] not in dict_IndexB.keys():
+            dict_IndexB[b[i]] = len(dict_IndexB)
+
+    prob = np.zeros((len(dict_IndexA), len(dict_IndexB)))
+
+    for i in range(a.size):
+        for j in range(b.size):
+            prob[dict_IndexA[a[i]], dict_IndexB[b[j]]] += 1
+    prob /= (a.size * b.size)
+
+    return prob
+
+def calculateMI_Plane(x, t, y, binCount = 24):
     # Push T into bins
-    T_bins = np.zeros((len(t), binCount))
-    for i in range(len(t)):
-        print(t[i])
-        T_bins[i] = np.histogram(t[i], bins=binCount, range=(-1.0, 1.0))
-    print (T_bins)
+    # T_bins = np.zeros((len(t), binCount))
+    T_Nums = np.zeros(len(t))
+    X_Nums = np.zeros(len(x))
+    Y_Nums = np.zeros(len(y))
 
-    return (len(t), len(t))
+    for i in range(len(t)):
+        # T_bins[i] = np.histogram(t[i], bins=binCount, range=(-1.0, 1.0))[0] # throw out bin edges
+        T_Nums[i] = array_to_number(np.histogram(t[i], bins=binCount, range=(-1.0, 1.0))[0])
+        X_Nums[i] = array_to_number(x[i])
+        Y_Nums[i] = array_to_number(y[i])
+        # cprint("- X: %d" % X_Nums[i], 'green')
+        # cprint("- T: %d" % T_Nums[i], 'yellow')
+        # cprint("- Y: %d" % Y_Nums[i], 'red')
+
+    # probXT = calculate_jointProb(X_Nums, T_Nums)
+    # probTY = calculate_jointProb(T_Nums, Y_Nums)
+
+    # cprint(probXT, 'yellow')
+    # cprint(probTY, 'cyan')
+
+    # return (mutual_info_score(None, None, contingency=probXT), mutual_info_score(None, None, contingency=probTY))
+    return (mutual_info_score(X_Nums, T_Nums), mutual_info_score(T_Nums, Y_Nums))
 
 #Variable used for calculation
 weight = []
@@ -101,40 +179,86 @@ train_op = optimizer.minimize(loss_op)
 # Initializing the variables
 init = tf.global_variables_initializer()
 
-def train_network(num_epochs, verbose=True, printInterval=1, calculateMI=True):
+def train_network(num_epochs, verbose=True, printInterval=100, calculateMI=True, num_loop=1, MI_Interval=20):
     with tf.Session() as sess:
-        sess.run(tf.global_variables_initializer())
-        training_losses = []
-        for idx, epoch in enumerate(gen_epochs(num_epochs)):
-            training_loss = 0
+
+        MI_Plane_XT = []
+        MI_Plane_TY = []
+        if calculateMI:
+            for i in range(len(layer) - 1):
+                MI_Plane_XT.append([])
+                MI_Plane_TY.append([])
+                for j in range(dataCount // batch_size // MI_Interval):
+                    MI_Plane_XT[i].append(0)
+                    MI_Plane_TY[i].append(0)
+
+        for loopCount in range(num_loop):
+            sess.run(tf.global_variables_initializer())
+            training_losses = []
+
             if verbose:
-                print("\nEPOCH", idx)
-            for step, (Xdata, Ydata) in enumerate(epoch):
-                _, c = \
-                    sess.run([train_op,
-                              loss_op],
-                                  feed_dict={X:Xdata, Y:Ydata})
-                training_loss += c
+                print(colored("\n*****\nLOOP %d Begins\n*****" % (loopCount + 1), 'cyan', attrs=['bold']))
 
-                #calculate Mutual Information
-                if calculateMI:
-                    layerData = \
-                        sess.run(layer, feed_dict={X:Xdata, Y:Ydata})
-                    # print (layerData[0])
+            for idx, epoch in enumerate(gen_epochs(num_epochs)):
+                training_loss = 0
+                if verbose:
+                    cprint("EPOCH %d" % idx, 'magenta')
+                for step, (Xdata, Ydata) in enumerate(epoch):
+                    _, c = \
+                        sess.run([train_op,
+                                  loss_op],
+                                      feed_dict={X:Xdata, Y:Ydata})
+                    training_loss += c
 
-                    for layerId in range(len(layerData) - 1):
-                        (IX_T, IT_Y) = calculateMI_Plane(Xdata, layerData[layerId], Ydata)
-                        print ("X: ", IX_T, " Y: ", IT_Y)
+                    #calculate Mutual Information
+                    if calculateMI and step % MI_Interval == 0:
+                        layerData = \
+                            sess.run(layer, feed_dict={X:Xdata, Y:Ydata})
+                        # print (layerData[0])
 
-                #show training status
-                if step % printInterval == 0 and step > 0:
-                    if verbose:
-                        print("Average loss at step", step,
-                              "for last ", printInterval, " step(s):", training_loss/printInterval)
-                    training_losses.append(training_loss/printInterval)
-                    training_loss = 0
-    return training_losses
+                        for layerId in range(len(layerData) - 1):
+                            (IX_T, IT_Y) = calculateMI_Plane(Xdata, layerData[layerId], Ydata)
+                            MI_Plane_XT[layerId][step // MI_Interval] += IX_T
+                            MI_Plane_TY[layerId][step // MI_Interval] += IT_Y
 
-training_losses = train_network(numEpochs)
+                            if step % printInterval == 0:
+                                print(colored("MI Plane position of Layer #%d: " % (layerId + 1), 'green') + colored("X: %lf, Y: %lf" % (IX_T, IT_Y), 'blue'))
+
+                    #show training status
+                    if step % printInterval == 0 and step > 0:
+                        if verbose:
+                            print(colored("Average loss at step %d for last %d step(s): " % (printInterval, step), 'yellow') + colored("%lf" % (training_loss/printInterval), 'red'))
+                        training_losses.append(training_loss/printInterval)
+                        training_loss = 0
+
+        if calculateMI:
+            for i in range(len(layer) - 1):
+                for j in range(dataCount // batch_size // MI_Interval):
+                    MI_Plane_XT[i][j] /= num_loop
+                    MI_Plane_TY[i][j] /= num_loop
+
+    return (training_losses, MI_Plane_XT, MI_Plane_TY)
+
+(training_losses, miX, miY) = train_network(numEpochs, num_loop = num_loop, MI_Interval = MI_Interval)
 plt.plot(training_losses)
+
+plt.show()
+plt.clf()
+plt.cla()
+plt.close()
+plt.close('all')
+
+#draw the MI plot
+
+colorLayer = []
+totalStepsMI = dataCount // batch_size // MI_Interval
+colorMaps = [cm.Greys, cm.Blues, cm.YlOrBr, cm.RdPu, cm.YlGn]
+for i in range(len(layer) - 1):
+    colorLayer.append([])
+    for j in range(totalStepsMI):
+        colorLayer[i].append(colorMaps[i % len(colorMaps)](j / totalStepsMI))
+
+for i in range(len(layer) - 1):
+    plt.scatter(miX[i], miY[i], c=colorLayer[i], zorder=2)
+    plt.plot(miX[i], miY[i], c=[0.3, 0.3, 0.3], ls='-', zorder=1)
 plt.show()
